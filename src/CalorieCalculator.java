b4b06ce5fd1aefd63ba92a2145a517018512d4cb
Petar Iliev <petariliev2002@gmail.com>
2022-01-07 22:16:57 +0100

    Added user variables

--- src/CalorieCalculator.java
+++ src/CalorieCalculator.java
@@ -3,6 +3,5 @@ public class CalorieCalculator {
     public static void main(String[] args) {
 
 
-
     }
 }
---
+++ src/User.java
@@ -0,0 +1,9 @@
+public class User {
+
+    protected String gender;
+    protected int age;
+    protected int height;
+    protected double weight;
+    protected String goal;
+
+}
